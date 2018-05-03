f762db30dd0c2351f6d17051af9eef84823acbb6
David Ebbo <david.ebbo@gmail.com>
2018-05-02 18:15:01 -0700

    Add project files.

---
+++ TimerTriggerProcessor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimerTriggerProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
